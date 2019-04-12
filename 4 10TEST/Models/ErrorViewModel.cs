533a3d92e5f076da218ca5dddecda6de426c5898
Sarah Weiss <suriw22@gmail.com>
2019-04-11 20:50:44 -0400

    Add project files.

---
+++ 4 10TEST/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _4_10TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
