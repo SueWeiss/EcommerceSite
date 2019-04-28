079a911beedb67a2b6e95ed0a54991427b59baec
Sarah Weiss <suriw22@gmail.com>
2019-04-28 15:02:38 -0400

    commit

--- 4 10TEST/Controllers/HomeController.cs
+++ 4 10TEST/Controllers/HomeController.cs
@@ -3,31 +3,91 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using _4_10TEST.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Library.Data;
+using Microsoft.AspNetCore.Http;
 
 namespace _4_10TEST.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+        private IHostingEnvironment _environment;
+        private string _connectionString;
+
+        public HomeController(IHostingEnvironment environment,
+            IConfiguration configuration)
         {
-            return View();
+            _environment = environment;
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
+        
+        public IActionResult Index(int id)
+        {
+            var mgr = new Manager(_connectionString);
+            IndexVeiwModels vm = new IndexVeiwModels();
+            vm.AllCategories = mgr.GetCategories();
+            vm.Products = mgr.GetProductsForCat(id);
+            return View(vm);
+        }
+        public IActionResult JasonProducts(int id)
+        {
+            var mgr = new Manager(_connectionString);
+            return Json(mgr.GetProductsForCat(id));
         }
 
-        public IActionResult About()
+        public IActionResult Products(int id)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            Manager mgr = new Manager(_connectionString);
+            return View(mgr.GetProductsById(id));
         }
 
-        public IActionResult Contact()
+        [HttpPost]
+        public IActionResult addToCart(int amount, int productId)
         {
-            ViewData["Message"] = "Your contact page.";
+            Manager mgr = new Manager(_connectionString);
 
-            return View();
+            string MyCart = HttpContext.Session.GetString("CartId");
+            if (MyCart == null)
+            {
+                MyCart = mgr.NewCart().ToString();
+                HttpContext.Session.SetString("CartId", $"{MyCart}");
+            }
+            mgr.AddItemsToCart(MyCart, productId, amount);
+            return Json(amount);
         }
+
+        public IActionResult MyCart()
+        {
+            Manager mgr = new Manager(_connectionString);
+            string MyCartId = HttpContext.Session.GetString("CartId");
+            CartVeiwModels cvm = new CartVeiwModels
+            {
+                CartTotal = mgr.GetCartTotal(int.Parse(MyCartId)),
+                CartItems = mgr.GetCartItems(int.Parse(MyCartId))
+            };
+            return View(cvm);
+        }
+        public IActionResult DeleteFromCart(int ItemId)
+        {
+            Manager mgr = new Manager(_connectionString);
+            string MyCartId = HttpContext.Session.GetString("CartId");
+            mgr.DeleteFromCart(ItemId,int.Parse(MyCartId));
+            return Redirect("/home/MyCart");
+        }
+        public IActionResult EditItem(int ProductId, int Quantity)
+        {
+            Manager mgr = new Manager(_connectionString);
+            string MyCartId = HttpContext.Session.GetString("CartId");
+            mgr.EditItem(ProductId, Quantity, int.Parse(MyCartId));
+            return Redirect("/home/MyCart");
+        }
+
+
 
         public IActionResult Privacy()
         {
